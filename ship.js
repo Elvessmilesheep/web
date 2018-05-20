51378a0c7e1385bf0c9f4886f14dd8f6a689daed
Elvessmilesheep <37857214+Elvessmilesheep@users.noreply.github.com>
2018-05-20 16:09:11 +0800

    Add files via upload

--- ship.js
+++ ship.js
@@ -12,6 +12,34 @@ var view = {
         cell.getAttribute("class","miss")
     }
 }
+var ships = [
+    {location :["10","20","30"],hits : ["","",""]},
+    {location :["32","33","34"],hits : ["","",""]},
+    {location :["63","64","65"],hits:["","","hit"]}
+];
+var model = {
+    borderSize:7,
+    numShips:3,
+    shipLength:3,
+    shipsSunk:0,
+ships = [
+    {location :["06","16","26"],hits : ["","",""]},
+    {location :["24","34","44"],hits : ["","",""]},
+    {location :["10","11","12"],hits:["","","hit"]}
+],
+fire: function (guess) {
+    for(var i=0;i < this.numShips;i++){
+        var ship = this.ships[i];
+        locations = ship.locations;
+        var index = locations.indexof(guess);
+        if(index>=0){
+            ship.hits[index] = "hit";
+            return true;
+        }
+    }
+    return false;
+}
+}
 view.displayMiss("00");
 view.displayHit("34");
 view.displayMiss("55");
